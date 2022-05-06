349ff8d99ff18342bfb48d23643c35c70ed66323
luyan <102529381+luyan0422@users.noreply.github.com>
2022-05-06 22:52:13 +0800

    Create 11461 - Square Numbers.cpp

---
+++ zerojudge/UVa/11461 - Square Numbers.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int from = 0, end = 0;
+    while(1){
+        int j,count = 0;
+        cin >> from >> end;
+        if(from == 0 && end ==0){
+            break;
+        }
+        for(j = 1; j * j < from; j++);
+        for(; j * j <= end ; j++){
+            count++;
+        } 
+        cout << count << endl;
+    }
+    return 0;
+}
