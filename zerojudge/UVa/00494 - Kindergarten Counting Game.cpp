39ca7966962237cd667c3e5b1ab743beacec7e24
luyan <102529381+luyan0422@users.noreply.github.com>
2022-05-09 11:12:15 +0800

    Create 00494 - Kindergarten Counting Game.cpp
    應該可以再簡化

---
+++ zerojudge/UVa/00494 - Kindergarten Counting Game.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<cstring>
+#include<cctype>
+using namespace std;
+void CountWord(char n[]);
+
+int main(){
+    string n;
+    while(getline(cin, n)){
+        int count = 0;
+        
+        for(int i = 1; i < n.length(); i++){
+            if((!isalpha(n[i])) && isalpha(n[i-1])){
+                count++;
+            }
+        }
+        if(isalpha(n[n.length() - 1])){
+            count++;
+        }
+        cout << count  << "\n";
+
+    }
+}
