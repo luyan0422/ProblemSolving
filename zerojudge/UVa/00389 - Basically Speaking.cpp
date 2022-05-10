81047b0c998eec0e9c5f71cb76a486d8b6fab59d
luyan <102529381+luyan0422@users.noreply.github.com>
2022-05-10 16:56:16 +0800

    Create 00389 - Basically Speaking.cpp

---
+++ zerojudge/UVa/00389 - Basically Speaking.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+#include<cmath>
+#include<cstdlib>
+using namespace std;
+
+int main(){
+    char num[8];
+    num[8] = '\0';
+    int a = 0, b= 0; // a 為轉換前基底 b為轉換後基底
+
+    while(cin >> num >> a >> b){
+        int j = -1, k = -1, count = 0; //count為一次轉換後的數字
+        for(int i = 0; i < 7 && num[i] != '\0'; i++){
+            k++;
+        } // 判斷有幾位輸入數字
+
+        for(int i = k; i >= 0; i--){ 
+            count += 
+                pow(a, k - i) * (num[i] > 64 ? num[i] - 55 : num[i] - 48);
+        }
+
+        for(int i = 0; i < 7 && count != 0; i++){
+            num[i] = (count % b) > 9 ? (count % b) + 55 : (count % b) + 48;
+            count = count / b;
+            j++;
+        } //轉換後存入
+
+        if(count != 0) {
+            cout << "ERROR";
+        }
+        else{
+            for(int i = 6; i >= 0; i--){
+                if(i > j) cout << " ";
+                else cout << num[i];
+            }
+        }  
+        cout << "\n";      
+    } 
+    return 0;
+}
+
+
