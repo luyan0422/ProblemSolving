535ea78ab3633348e27da39b87401e596c976674
luyan <102529381+luyan0422@users.noreply.github.com>
2022-05-16 14:37:05 +0800

    Update 11827 - Maximum GCD.cpp
    GCD中傳入目前最大的gcd 當參數 有變快

--- zerojudge/UVa/11827 - Maximum GCD.cpp
+++ zerojudge/UVa/11827 - Maximum GCD.cpp
@@ -2,12 +2,12 @@
 #include<sstream>
 using namespace std;
 void FIND();
-int GCD(int i, int j);  //輾轉相除法求最大公因數
+int GCD(int i, int j, int max);
 
 int main(){
     int N;
     scanf("%d",&N);
-    getchar();  //第一行後面的換行要先解決 不然會錯誤
+    getchar();  //第一行後面的換行
     for(int i = 1; i <= N; i++){
         FIND();
     }
@@ -15,20 +15,20 @@ int main(){
 }
 
 void FIND(){
-    stringstream stream;  //宣告一個串流
+    stringstream stream;
     string s;
     int a[120] = {0};
     int number, i = 0, maxgcd = 0;
     getline(cin, s);  //讀取一整行
     stream << s;   //把s寫入stream
-    while(stream >> number){ //string 轉成 int
+    while(stream >> number){
         a[i] = number;
         i++;
     }
     for(int m = 0; m < i; m++){
         for(int k = 0; k < i; k++){
             if(m == k) continue;
-            int temp = GCD(a[m], a[k]);
+            int temp = GCD(a[m], a[k], maxgcd);
             if(temp > maxgcd){
                 maxgcd = temp;
             }
@@ -37,14 +37,15 @@ void FIND(){
     cout << maxgcd << "\n";
 } 
 
-int GCD(int i, int j){
-    if(i < j){
-        return GCD(j, i);
+int GCD(int i, int j, int max){
+    if(i < max || j < max) return 0;
+    else if(i < j){
+        return GCD(j, i, max);
     }
     else{
         if(i % j == 0){
             return j;
         }
-        else return GCD(j, i % j);
+        else return GCD(j, i % j ,max);
     }
 }
